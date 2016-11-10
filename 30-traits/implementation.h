235f318a4c4fab9c6fa5362ebf2f541901cbb5e4
Szymon Wasilewski <szymonwasilewski01@gmail.com>
2016-11-10 12:21:34 +0100

    first, added templates for 32-interesting test-case

--- 30-traits/implementation.h
+++ 30-traits/implementation.h
@@ -1,14 +1,5 @@
 #pragma once
 
 template<typename T, typename T1, typename T2, typename T3>
-struct is_any_of : std::false_type {};
-
-template<typename T, typename T1, typename T2>
-struct is_any_of<T, T, T1, T2> : std::true_type {};
-
-template<typename T, typename T1, typename T2>
-struct is_any_of<T, T1, T, T2> : std::true_type {};
-
-template<typename T, typename T1, typename T2>
-struct is_any_of<T, T1, T2, T> : std::true_type {};
-
+struct is_any_of : std::disjunction<std::is_same<T, T1>, std::is_same<T,T2>, std::is_same<T, T3>>
+{};
