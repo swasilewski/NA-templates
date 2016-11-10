047a6f6059a203fd9ac4f4d5a3ea6dfcffdcadc0
Szymon Wasilewski <szymonwasilewski01@gmail.com>
2016-11-10 12:17:12 +0100

    first, is_any_of implementation using template specializations only

--- 30-traits/implementation.h
+++ 30-traits/implementation.h
@@ -1,5 +1,14 @@
 #pragma once
 
 template<typename T, typename T1, typename T2, typename T3>
-struct is_any_of;
+struct is_any_of : std::false_type {};
+
+template<typename T, typename T1, typename T2>
+struct is_any_of<T, T, T1, T2> : std::true_type {};
+
+template<typename T, typename T1, typename T2>
+struct is_any_of<T, T1, T, T2> : std::true_type {};
+
+template<typename T, typename T1, typename T2>
+struct is_any_of<T, T1, T2, T> : std::true_type {};
 
