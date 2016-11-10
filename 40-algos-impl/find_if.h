88d5dfa69b9a1220c409cb42e96a69893dabf5e4
Szymon Wasilewski <szymonwasilewski01@gmail.com>
2016-11-10 13:00:47 +0100

    added find_if implementation

--- 40-algos-impl/find_if.h
+++ 40-algos-impl/find_if.h
@@ -2,7 +2,17 @@
 
 namespace training
 {
-template<typename InputIt, typename UnaryPredicate>
-InputIt find_if(InputIt begin, InputIt end, UnaryPredicate pred);
+	template<typename InputIt, typename UnaryPredicate>
+	InputIt find_if(InputIt begin, InputIt end, UnaryPredicate pred)
+	{
+		for(InputIt it=begin;it<end;it++)
+		{
+			if(pred(*it))
+			{
+				return it;
+			}
+		}
+		return end;
+	}
 }
 
